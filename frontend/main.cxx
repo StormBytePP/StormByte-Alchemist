2bce4864b6c0b1da76bd57098a4ffbf71b083c3b
David C. Manuelda <StormByte@gmail.com>
2022-09-14 16:36:49 +0200

    Deleted comments to clean code

--- frontend/main.cxx
+++ frontend/main.cxx
@@ -1,12 +1,7 @@
 #include "application.hxx"
-#include "ffmpeg/ffmpeg.hxx"
 
 using namespace StormByte::VideoConvert;
 
 int main(int argc, char **argv) {
 	return Application::get_instance().run(argc, argv);
-
-	//FFmpeg f1(1, "/tmp/u.mkv"), f2 = f1;
-
-	//return 0;
 }
