981f1695b5e9d79a511c73e4035fc8dd599fda2e
David C. Manuelda <StormByte@gmail.com>
2022-09-19 14:29:21 +0200

    post_run_actions must always return either HALT_OK either HALT_ERROR

--- lib/task/base.cxx
+++ lib/task/base.cxx
@@ -1,5 +1,7 @@
 #include "base.hxx"
 
+#include <assert.h>
+
 using namespace StormByte::VideoConvert;
 
 Task::Base::Base():m_status(STOPPED) {}
@@ -11,18 +13,23 @@ Task::STATUS Task::Base::run() noexcept {
 
 Task::STATUS Task::Base::run(std::optional<pid_t>& worker) noexcept {
 	STATUS status = HALT_ERROR;
+
 	m_start = std::chrono::steady_clock::now();
 	if (pre_run_actions() == RUNNING)
 		status = do_work(worker);
 	// Post run action might change status
 	status = post_run_actions(status);
 	m_end = std::chrono::steady_clock::now();
+	
+	assert(status == HALT_OK || status == HALT_ERROR);
 	return status;
 }
 
 Task::STATUS Task::Base::pre_run_actions() noexcept { return RUNNING; }
 
-Task::STATUS Task::Base::post_run_actions(const STATUS& prev_status) noexcept { return prev_status; }
+Task::STATUS Task::Base::post_run_actions(const STATUS& prev_status) noexcept {
+	return prev_status == RUNNING ? HALT_OK : prev_status;
+}
 
 std::string Task::Base::elapsed_time_string() const {
 	/* NOTE: Until C++20's <format> support is complete, I just use this aproach */
