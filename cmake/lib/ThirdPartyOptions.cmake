fbb16e326fa66af4f4c73f7985f02d34f4913721
David C. Manuelda <StormByte@gmail.com>
2024-03-06 21:41:40 +0100

    Added jsoncpp bundled

--- cmake/lib/ThirdPartyOptions.cmake
+++ cmake/lib/ThirdPartyOptions.cmake
@@ -1,3 +1,4 @@
 option(WITH_BUNDLED_SQLITE	"Use bundled static sqlite library"		ON)
+option(WITH_BUNDLED_JSONCPP	"Use bundled static jsoncpp library"	ON)
 
 add_subdirectory(${ALCHEMY_DIR}/thirdparty)
