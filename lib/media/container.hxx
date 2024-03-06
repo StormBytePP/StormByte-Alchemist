fc0b105718ad724770c701c9601c85bf921df110
David C. Manuelda <StormByte@gmail.com>
2024-03-06 22:12:50 +0100

    Add container stub

---
+++ lib/media/container.hxx
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "codec.hxx"
+
+#include <list>
+
+namespace Alchemist::Media {
+	class DLL_PUBLIC Container {
+		public:
+			enum Type:unsigned short {
+				AVI, MKV, MP4, OGG, // Video
+				AAC, DTS, MP2, MP3, OPUS, // Audio
+				BMP, GIF, JPG, PNG, TIFF, WEBP // Image
+			};
+
+			Container()														= default;
+			Container(const Container&)										= default;
+			Container(Container&&)											= default;
+			Container& operator=(const Container&)							= default;
+			Container& operator=(Container&&)								= default;
+			virtual ~Container()											= default;
+
+			virtual std::list<Codec::Type> get_supported_codecs() const		= 0;
+	};
+}
