56ff211c42a521f469a8ed884002a757a908b6e0
David C. Manuelda <StormByte@gmail.com>
2024-03-07 16:42:47 +0100

    .

--- lib/media/info.hxx
+++ lib/media/info.hxx
@@ -9,22 +9,26 @@
 
 namespace Alchemist::Media {
 	class DLL_PUBLIC Info {
-		struct Item {
-			unsigned short id;
-			std::string short_name;
-			std::string long_name;
-		};
-		class Codec {
-			static const std::list<Media::Codec::Type>		Available;
-			static const std::list<Item>					All;
-		};
-		class Decoder {
-			static const std::list<Media::Decoder::Type>	Available;
-			static const std::list<Item>					All;
-		};
-		class Encoder {
-			static const std::list<Media::Encoder::Type>	Available;
-			static const std::list<Item>					All;
-		};
+		public:
+			struct Item {
+				unsigned short id;
+				std::string short_name;
+				std::string long_name;
+			};
+			class Codec {
+				public:
+					static const std::list<Media::Codec::Type>		Available;
+					static const std::list<Item>					All;
+			};
+			class Decoder {
+				public:
+					static const std::list<Media::Decoder::Type>	Available;
+					static const std::list<Item>					All;
+			};
+			class Encoder {
+				public:
+					static const std::list<Media::Encoder::Type>	Available;
+					static const std::list<Item>					All;
+			};
 	};
 }
--- test/main.cxx
+++ test/main.cxx
@@ -1,11 +1,11 @@
-#include <media/codec.hxx>
-#include <media/decoder.hxx>
-#include <media/encoder.hxx>
+#include <media/info.hxx>
 
 #include <iostream>
 
 int main() {
 	std::cout << "Available codecs: " << std::endl;
-	
+	for (auto item: Alchemist::Media::Info::Codec::All) {
+		std::cout << item.short_name << ": " << item.long_name << std::endl;
+	}
 	return 0;
 }
