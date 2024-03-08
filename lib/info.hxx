95691ee83d57640d11923cd79ebf9b09e2d1c332
David C. Manuelda <StormByte@gmail.com>
2024-03-08 18:46:41 +0100

    Solved a typo

--- lib/info.hxx
+++ lib/info.hxx
@@ -13,12 +13,12 @@ namespace Alchemist {
 			static const std::string 			DLL_PUBLIC Version;
 			static const std::string 			DLL_PUBLIC URI;
 
-			static const std::filesystem::path 	DLL_PUBLIC ffmpeg_path();
-			static const std::filesystem::path 	DLL_PUBLIC ffprobe_path();
+			static const std::filesystem::path  DLL_PUBLIC ffmpeg_path();
+			static const std::filesystem::path  DLL_PUBLIC ffprobe_path();
 
 		private:
 			#ifdef _WIN32
-			static const std::filesystem::path DLL_LOCAL 0
+			static const std::filesystem::path DLL_LOCAL current_path();
 			#endif
 
 	};
