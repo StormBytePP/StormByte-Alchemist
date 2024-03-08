cb050b47bb91c9b3456c97270a6845b5db700b9d
David C. Manuelda <StormByte@gmail.com>
2024-03-08 19:26:03 +0100

    Reworked Info

--- lib/media/info.hxx
+++ lib/media/info.hxx
@@ -4,31 +4,31 @@
 #include "decoder.hxx"
 #include "encoder.hxx"
 
+#include <filesystem>
 #include <list>
 #include <string>
 
 namespace Alchemist::Media {
-	class DLL_PUBLIC Info {
-		public:
-			struct Item {
-				unsigned short id;
-				std::string short_name;
-				std::string long_name;
-			};
-			class Codec {
-				public:
-					static const std::list<Media::Codec::Type>		DLL_PUBLIC Available;
-					static const std::list<Item>					DLL_PUBLIC All;
-			};
-			class Decoder {
-				public:
-					static const std::list<Media::Decoder::Type>	DLL_PUBLIC Available;
-					static const std::list<Item>					DLL_PUBLIC All;
-			};
-			class Encoder {
-				public:
-					static const std::list<Media::Encoder::Type>	DLL_PUBLIC Available;
-					static const std::list<Item>					DLL_PUBLIC All;
-			};
+	struct DLL_PUBLIC Info {
+		struct Item {
+			unsigned short id;
+			std::string short_name;
+			std::string long_name;
+		};
+		class Codec {
+			public:
+				static const std::list<Media::Codec::Type>		DLL_PUBLIC Available;
+				static const std::list<Item>					DLL_PUBLIC All;
+		};
+		class Decoder {
+			public:
+				static const std::list<Media::Decoder::Type>	DLL_PUBLIC Available;
+				static const std::list<Item>					DLL_PUBLIC All;
+		};
+		class Encoder {
+			public:
+				static const std::list<Media::Encoder::Type>	DLL_PUBLIC Available;
+				static const std::list<Item>					DLL_PUBLIC All;
+		};
 	};
 }
