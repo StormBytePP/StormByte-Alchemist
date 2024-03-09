97e665100d1bb8d8ecc838e87d53b5d0d1cc4121
David C. Manuelda <StormByte@gmail.com>
2024-03-09 15:39:05 +0100

    Added tests

--- test/main.cxx
+++ test/main.cxx
@@ -73,11 +73,47 @@ void test4() {
 	exec1.wait();
 }
 
+void test5() {
+	std::cout << "Test 5: " << std::flush;
+	const std::string expected_output = "123";
+	std::string real_output;
+	Alchemist::Executable exec1("/usr/bin/echo", "");
+	Alchemist::Executable exec2("/usr/bin/echo", "");
+	exec1 << "1" << "2" << "3" << Alchemist::Executable::EoF;
+	real_output << exec1;
+
+	exec1 >> exec2;
+
+	
+	test_result(expected_output, real_output);
+
+	exec2.wait();
+	exec1.wait();
+}
+
+void test6() {
+	std::cout << "Test 6: " << std::flush;
+	const std::string expected_output = "123";
+	std::string real_output;
+	Alchemist::Executable exec1("/usr/bin/echo", "");
+	Alchemist::Executable exec2("/usr/bin/echo", "");
+	exec1 << "1" << "2" << "3" << Alchemist::Executable::EoF;
+	exec1 >> exec2;
+	
+	real_output << exec2;
+	test_result(expected_output, real_output);
+
+	exec2.wait();
+	exec1.wait();
+}
+
 int main() {
 	test1();
 	test2();
 	test3();
 	test4();
+	test5();
+	test6();
 
 	return 0;
 }
