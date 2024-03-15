d0de425aa9048e6739fce182fc2ff728bd37698a
David C. Manuelda <StormByte@gmail.com>
2024-03-15 04:08:23 +0100

    Padded features

--- lib/media/attribute.hxx
+++ lib/media/attribute.hxx
@@ -4,10 +4,10 @@
 
 namespace Alchemist::Media {
 	enum DLL_PUBLIC Attribute:unsigned short {
-		DEFAULT		= 0x001,	/* Default option */
-		EXPENSIVE	= 0x002,	/* Computational expensive */
-		HARDWARE	= 0x004,	/* Hardware accelerated */
-		OLD			= 0x008,	/* Considered old nowadays */
-		QUALITY		= 0x010,	/* Produces more quality */
+		DEFAULT		= 0x001,	/* Default option 			*/
+		EXPENSIVE	= 0x002,	/* Computational expensive 	*/
+		HARDWARE	= 0x004,	/* Hardware accelerated 	*/
+		OLD			= 0x008,	/* Considered old nowadays 	*/
+		QUALITY		= 0x010,	/* Produces more quality 	*/
 	};
 }
