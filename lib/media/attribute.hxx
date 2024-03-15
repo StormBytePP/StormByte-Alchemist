638d962e0994825eeffc6d7b17084ca145325a43
David C. Manuelda <StormByte@gmail.com>
2024-03-15 03:58:06 +0100

    Attribute reorder

--- lib/media/attribute.hxx
+++ lib/media/attribute.hxx
@@ -5,9 +5,9 @@
 namespace Alchemist::Media {
 	enum DLL_PUBLIC Attribute:unsigned short {
 		DEFAULT		= 0x001,	/* Default option */
-		HARDWARE	= 0x002,	/* Hardware accelerated */
-		EXPENSIVE	= 0x004,	/* Computational expensive */
-		QUALITY		= 0x008,	/* Produces more quality */
-		OLD			= 0x010,	/* Considered old nowadays */
+		EXPENSIVE	= 0x002,	/* Computational expensive */
+		HARDWARE	= 0x004,	/* Hardware accelerated */
+		OLD			= 0x008,	/* Considered old nowadays */
+		QUALITY		= 0x010,	/* Produces more quality */
 	};
 }
