8c0cb2124317af487c4294f3badeac8472754466
David C. Manuelda <StormByte@gmail.com>
2024-03-19 17:03:54 +0100

    Created package target for Windows

--- cmake/ConfigQtInstaller.cmake
+++ cmake/ConfigQtInstaller.cmake
@@ -1,37 +1,6 @@
-file(GENERATE OUTPUT "${CMAKE_BINARY_DIR}/installer/config.xml" CONTENT
-"<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<Installer>
-    <Name>${PROJECT_NAME}</Name>
-    <Version>${PROJECT_VERSION}</Version>
-    <Title>${PROJECT_NAME}</Title>
-    <Publisher>StormByte &lt;stormbyte@gmail.com&gt;</Publisher>
-    <StartMenuDir>${PROJECT_NAME}</StartMenuDir>
-    <TargetDir>@ApplicationsDir@/Alchemist</TargetDir>
-	<AllowSpaceInPath>true</AllowSpaceInPath>
-	<WizardStyle>Aero</WizardStyle>
-	<Translations>
-        <Translation>es.qm</Translation>
-    </Translations>
-</Installer>"
+add_custom_target(package
+	${QT_DIR}/../../../../../Tools/QtInstallerFramework/4.7/bin/binarycreator.exe --offline-only -c config/config.xml -p packages install.exe
+	COMMAND echo "Installable exe created in ${CMAKE_SOURCE_DIR}/dist/install.exe"
+	WORKING_DIRECTORY "${CMAKE_INSTALL_PREFIX}/../../.."
+	DEPENDS install
 )
-install(FILES "${CMAKE_BINARY_DIR}/installer/config.xml" DESTINATION "${CMAKE_INSTALL_PREFIX}/../../../config" )
-
-file(GENERATE OUTPUT "${CMAKE_BINARY_DIR}/installer/package.xml" CONTENT
-"<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<Package>
-	<DisplayName>${PROJECT_NAME}</DisplayName>
-	<Description>Install Alchemist</Description>
-	<Version>${PROJECT_VERSION}</Version>
-	<ReleaseDate>2024-03-19</ReleaseDate>
-	<Licenses>
-		<License name=\"GNU General Public License v3.0\" file=\"license.txt\" />
-	</Licenses>
-	<Default>script</Default>
-	<Script>installscript.qs</Script>
-	<UserInterfaces>
-		<UserInterface>page.ui</UserInterface>
-	</UserInterfaces>
-	<RequiresAdminRights>true</RequiresAdminRights>
-</Package>"
-)
-install(FILES "${CMAKE_BINARY_DIR}/installer/package.xml" DESTINATION "${CMAKE_INSTALL_PREFIX}/../../../packages/com.StormByte.Alchemist/meta" )
