aaa1112d1e19962d309df0818563627b8e41baf8
David C. Manuelda <StormByte@gmail.com>
2024-03-19 18:49:30 +0100

    Updated deployment script

--- deploy_windows.bat
+++ deploy_windows.bat
@@ -1,6 +1,8 @@
 @echo off
 rmdir /S /Q build 2>null
+rmdir /S /Q dist\packages\org.StormByte.Alchemist\data 2>null
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR=Ninja -DWITH_SYSTEM_JSONCPP=OFF -DWITH_SYSTEM_SQLITE=OFF -DCMAKE_INSTALL_PREFIX=..\dist\packages\org.StormByte.Alchemist\data\ ..
+ninja repository
 ninja package
