45de72efbb5ba78b41bb2e7e40ad39e45c5c34af
David C. Manuelda <StormByte@gmail.com>
2024-03-19 14:01:43 +0100

    Added base skeleton for installer

---
+++ dist/packages/com.StormByte.Alchemist/meta/es.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>Page</name>
+    <message>
+        <source>Install Alchemist</source>
+        <translation>Instalar Alchemist</translation>
+    </message>
+</context>
+</TS>
