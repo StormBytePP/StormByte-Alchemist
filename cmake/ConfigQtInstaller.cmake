8ba9c593a0dc2d7c227fb449d9163c8efc9815e5
David C. Manuelda <StormByte@gmail.com>
2024-03-19 18:43:18 +0100

    Added repository target

--- cmake/ConfigQtInstaller.cmake
+++ cmake/ConfigQtInstaller.cmake
@@ -1,6 +1,13 @@
 add_custom_target(package
-	${QT_DIR}/../../../../../Tools/QtInstallerFramework/4.7/bin/binarycreator.exe --offline-only -c config/config.xml -p packages install.exe
+	${QT_DIR}/../../../../../Tools/QtInstallerFramework/4.7/bin/binarycreator.exe -c config/config.xml -p packages -e org.StormByte.Alchemist install.exe
 	COMMAND echo "Installable exe created in ${CMAKE_SOURCE_DIR}/dist/install.exe"
+	WORKING_DIRECTORY "${CMAKE_INSTALL_PREFIX}/../../.."
+	DEPENDS install
+)
+
+add_custom_target(repository
+	${QT_DIR}/../../../../../Tools/QtInstallerFramework/4.7/bin/repogen.exe -p packages -i org.StormByte.Alchemist repository
+	COMMAND echo "Repository contents created in ${CMAKE_SOURCE_DIR}/dist/repository"
 	WORKING_DIRECTORY "${CMAKE_INSTALL_PREFIX}/../../.."
 	DEPENDS install
 )
--- deploy_windows.bat
+++ deploy_windows.bat
@@ -2,5 +2,5 @@
 rmdir /S /Q build 2>null
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR=Ninja -DWITH_SYSTEM_JSONCPP=OFF -DWITH_SYSTEM_SQLITE=OFF -DCMAKE_INSTALL_PREFIX=..\dist\packages\com.StormByte.Alchemist\data\ ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR=Ninja -DWITH_SYSTEM_JSONCPP=OFF -DWITH_SYSTEM_SQLITE=OFF -DCMAKE_INSTALL_PREFIX=..\dist\packages\org.StormByte.Alchemist\data\ ..
 ninja package
