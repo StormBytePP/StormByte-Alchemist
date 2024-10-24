6e7312a2499712b8b74825cd16718a923aa20733
StormBytePP <stormbyte@gmail.com>
2024-10-24 09:01:23 +0200

    Update opus.cxx

--- lib/ffmpeg/stream/audio/opus.cxx
+++ lib/ffmpeg/stream/audio/opus.cxx
@@ -3,6 +3,7 @@
 using namespace StormByte::VideoConvert;
 
 const std::string Stream::Audio::Opus::OPUS_DEFAULT_ENCODER	= "libopus";
+const int Stream::Audio::Opus::OPUS_DEFAULT_BITRATE_PER_CHANNEL = 128;
 
 Stream::Audio::Opus::Opus(const unsigned short& stream_id):Stream::Audio::Base(stream_id, OPUS_DEFAULT_ENCODER, Database::Data::film::stream::AUDIO_OPUS) {}
 
