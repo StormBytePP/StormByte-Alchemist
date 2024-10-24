db7b18eed3b9d500cd32d440b8ea9a4ecd9e312e
StormBytePP <stormbyte@gmail.com>
2024-10-24 09:04:24 +0200

    Update opus.cxx
    Improve channels support

--- lib/ffmpeg/stream/audio/opus.cxx
+++ lib/ffmpeg/stream/audio/opus.cxx
@@ -8,3 +8,9 @@ const int Stream::Audio::Opus::OPUS_DEFAULT_BITRATE_PER_CHANNEL = 128;
 Stream::Audio::Opus::Opus(const unsigned short& stream_id):Stream::Audio::Base(stream_id, OPUS_DEFAULT_ENCODER, Database::Data::film::stream::AUDIO_OPUS) {}
 
 Stream::Audio::Opus::Opus(unsigned short&& stream_id):Stream::Audio::Base(std::move(stream_id), OPUS_DEFAULT_ENCODER, Database::Data::film::stream::AUDIO_OPUS) {}
+
+void Stream::Audio::Opus::SetChannels(const int& channels) {
+  Stream::Audio::Base::SetChannels(channels);
+  // This is only a temp solution
+  m_bitrate = std::to_string(OPUS_DEFAULT_BITRATE_PER_CHANNEL * *m_channels) + "K";
+}
