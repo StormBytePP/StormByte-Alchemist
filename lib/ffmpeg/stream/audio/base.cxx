e90eb191ad00da37fe909640897164d4216f710b
StormBytePP <stormbyte@gmail.com>
2024-10-24 09:02:16 +0200

    Update base.cxx

--- lib/ffmpeg/stream/audio/base.cxx
+++ lib/ffmpeg/stream/audio/base.cxx
@@ -7,7 +7,5 @@ Stream::Audio::Base::Base(const unsigned short& stream_id, const std::string& en
 Stream::Audio::Base::Base(unsigned short&& stream_id, std::string&& encoder, Database::Data::film::stream::codec&& codec):Stream::Base::Base(std::move(stream_id), std::move(encoder), std::move(codec), 'a') {}
 
 void Stream::Audio::Base::SetChannels(const int& channels) {
-  // Temp: Set bitrate here until it is properly done
   m_channels = channels;
-  m_bitrate = std::to_string(96 * *m_channels) + "K";
 }
