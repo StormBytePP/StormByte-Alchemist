ceb5ee8a01dbe51843ad379cd6ba434ec04f759a
David C. Manuelda <StormByte@gmail.com>
2024-10-24 12:40:27 +0200

    Set bitrate hardcoded to fdk_aac

--- lib/ffmpeg/stream/audio/fdk_aac.cxx
+++ lib/ffmpeg/stream/audio/fdk_aac.cxx
@@ -5,15 +5,20 @@ using namespace StormByte::VideoConvert;
 const std::string Stream::Audio::FDKAAC::FDKAAC_DEFAULT_ENCODER	= "libfdk_aac";
 const std::string Stream::Audio::FDKAAC::FDKAAC_DEFAULT_PROFILE	= "aac_he";
 
-Stream::Audio::FDKAAC::FDKAAC(const unsigned short& stream_id):Stream::Audio::Base(stream_id, FDKAAC_DEFAULT_ENCODER, Database::Data::film::stream::AUDIO_FDKAAC), m_profile(FDKAAC_DEFAULT_PROFILE) {}
+Stream::Audio::FDKAAC::FDKAAC(const unsigned short& stream_id):Stream::Audio::Base(stream_id, FDKAAC_DEFAULT_ENCODER, Database::Data::film::stream::AUDIO_FDKAAC), m_profile(FDKAAC_DEFAULT_PROFILE) {
+	m_bitrate = "512k";
+}
 
-Stream::Audio::FDKAAC::FDKAAC(unsigned short&& stream_id):Stream::Audio::Base(std::move(stream_id), FDKAAC_DEFAULT_ENCODER, Database::Data::film::stream::AUDIO_FDKAAC), m_profile(FDKAAC_DEFAULT_PROFILE) {}
+Stream::Audio::FDKAAC::FDKAAC(unsigned short&& stream_id):Stream::Audio::Base(std::move(stream_id), FDKAAC_DEFAULT_ENCODER, Database::Data::film::stream::AUDIO_FDKAAC), m_profile(FDKAAC_DEFAULT_PROFILE) {
+	m_bitrate = "512k";
+}
 
 std::list<std::string> Stream::Audio::FDKAAC::ffmpeg_parameters() const {
 	std::list<std::string> result = Stream::Audio::Base::ffmpeg_parameters();
 
 	if (m_profile) {
 		result.push_back("-profile:" + ffmpeg_stream_pos());			result.push_back(*m_profile);
+		result.push_back("-cutoff:" + ffmpeg_stream_pos());				result.push_back("15000");
 	}
 
 	return result;
