063611189d64ecea86e122b562c1d6750ed09568
StormBytePP <stormbyte@gmail.com>
2024-10-24 08:49:35 +0200

    Update base.cxx
    Set bitrate to 96K * #channels when using Opus

--- lib/ffmpeg/stream/audio/base.cxx
+++ lib/ffmpeg/stream/audio/base.cxx
@@ -5,3 +5,9 @@ using namespace StormByte::VideoConvert;
 Stream::Audio::Base::Base(const unsigned short& stream_id, const std::string& encoder, const Database::Data::film::stream::codec& codec):Stream::Base::Base(stream_id, encoder, codec, 'a') {}
 
 Stream::Audio::Base::Base(unsigned short&& stream_id, std::string&& encoder, Database::Data::film::stream::codec&& codec):Stream::Base::Base(std::move(stream_id), std::move(encoder), std::move(codec), 'a') {}
+
+void Stream::Audio::Base::SetChannels(const int& channels) {
+  // Temp: Set bitrate here until it is properly done
+  m_channels = channels;
+  m_bitrate = std::to_string(96 * *m_channels) + "K";
+}
