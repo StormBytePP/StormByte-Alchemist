9c9df70b9c1d8a807c5a96b01a88670e91f4f1c3
David C. Manuelda <StormByte@gmail.com>
2024-10-28 21:05:48 +0100

    Fixed color for video

--- lib/media/video/color.cxx
+++ lib/media/video/color.cxx
@@ -10,18 +10,23 @@ m_prim(primaries), m_matrix(matrix), m_transfer(transfer), m_pix_fmt(pix_fmt) {}
 Color::Color(std::string&& primaries, std::string&& matrix, std::string&& transfer, std::string&& pix_fmt) noexcept:
 m_prim(std::move(primaries)), m_matrix(std::move(matrix)), m_transfer(std::move(transfer)), m_pix_fmt(std::move(pix_fmt)) {}
 
-const std::string& Color::GetPrimaries() const noexcept { return m_prim; }
+const std::optional<std::string>& Color::GetPrimaries() const noexcept { return m_prim; }
 
-const std::string& Color::GetMatrix() const noexcept { return m_matrix; }
+const std::optional<std::string>& Color::GetMatrix() const noexcept { return m_matrix; }
 
-const std::string& Color::GetTransfer() const noexcept { return m_transfer; }
+const std::optional<std::string>& Color::GetTransfer() const noexcept { return m_transfer; }
 
-const std::string& Color::GetPixelFormat() const noexcept { return m_pix_fmt; }
+const std::optional<std::string>& Color::GetPixelFormat() const noexcept { return m_pix_fmt; }
 
 std::list<std::string> Color::GetParameters() const {
-	return {
-		{ "colorprim=" + m_prim },
-		{ "colormatrix=" + m_matrix },
-		{ "transfer=" + m_transfer }
-	};
+	std::list<std::string> parameters;
+
+	if (m_prim)
+		parameters.push_back("colorprim=" + *m_prim);
+	if (m_matrix)
+		parameters.push_back("colormatrix=" + *m_matrix);
+	if (m_transfer)
+		parameters.push_back("transfer=" + *m_transfer);
+
+	return parameters;
 }
--- lib/media/video/color.hxx
+++ lib/media/video/color.hxx
@@ -10,21 +10,31 @@ namespace Alchemist::Media::Video {
 		public:
 			static DLL_PUBLIC const Color DEFAULT;
 
+			Color()								= default;
 			Color(const std::string&, const std::string&, const std::string&, const std::string&);
 			Color(std::string&&, std::string&&, std::string&&, std::string&&) noexcept;
+			Color(std::string&&) noexcept;
 			Color(const Color&)					= default;
 			Color(Color&&) noexcept				= default;
 			Color& operator=(const Color&)		= default;
 			Color& operator=(Color&&) noexcept	= default;
 			~Color() noexcept					= default;
 
-			const std::string& 		GetPrimaries() const noexcept;
-			const std::string& 		GetMatrix() const noexcept;
-			const std::string& 		GetTransfer() const noexcept;
-			const std::string& 		GetPixelFormat() const noexcept;
-			std::list<std::string>	GetParameters() const;
+			const std::optional<std::string>&	GetPrimaries() const noexcept;
+			void								SetPrimaries(const std::string&);
+			void								SetPrimaries(std::string&&) noexcept;
+			const std::optional<std::string>& 	GetMatrix() const noexcept;
+			void								SetMatrix(const std::string&);
+			void								SetMatrix(std::string&&) noexcept;
+			const std::optional<std::string>& 	GetTransfer() const noexcept;
+			void								SetTransfer(const std::string&);
+			void								SetTransfer(std::string&&) noexcept;
+			const std::optional<std::string>&	GetPixelFormat() const noexcept;
+			void								SetPixelFormat(const std::string&);
+			void								SetPixelFormat(std::string&&) noexcept;
+			std::list<std::string>				GetParameters() const;
 
 		private:
-			std::string m_prim, m_matrix, m_transfer, m_pix_fmt;
+			std::optional<std::string> m_prim, m_matrix, m_transfer, m_pix_fmt;
 	};
 }
