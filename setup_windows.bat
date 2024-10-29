a54197e44d86b4a5ba9d8c834307d213972f23ab
David C. Manuelda <StormByte@gmail.com>
2024-10-30 00:21:54 +0100

    Updated windows setup file

--- setup_windows.bat
+++ setup_windows.bat
@@ -1,4 +1,8 @@
+@echo OFF
+
 rmdir /s /q build
+rmdir /s /q dist
 mkdir build
-cd build
-cmake -G Ninja -DWITH_SYSTEM_JSONCPP=OFF -DWITH_SYSTEM_SQLITE=OFF ..
+mkdir dist
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR=Ninja -DCMAKE_INSTALL_PREFIX=dist -DWITH_SYSTEM_JSONCPP=OFF -DWITH_SYSTEM_SQLITE=OFF -Bbuild -H.
