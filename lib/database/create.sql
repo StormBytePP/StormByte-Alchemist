10e244bbb9b0ec79d44eb3521979a584ce8edfe3
David C. Manuelda <StormByte@gmail.com>
2024-10-29 04:52:05 +0100

    Updated database structure

---
+++ lib/database/create.sql
@@ -0,0 +1,83 @@
+CREATE TABLE films (
+	film_id		INTEGER,
+	in_file		VARCHAR NOT NULL,
+	out_file	VARCHAR NOT NULL,
+	priority	SMALLINT NOT NULL,
+	in_process	BOOL NOT NULL DEFAULT FALSE,
+	is_enabled	BOOL NOT NULL DEFAULT TRUE,
+	PRIMARY KEY (film_id)
+);
+
+CREATE TABLE streams (
+	film_id		INTEGER,
+	stream_id	SMALLINT,
+	stream_type	CHAR NOT NULL,
+	title		VARCHAR NOT NULL,
+	lang		CHAR(3) NOT NULL,
+	PRIMARY KEY (film_id, stream_id),
+	FOREIGN KEY (stream_id) REFERENCES films(film_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE stream_metadata_audio (
+	film_id		INTEGER,
+	stream_id	SMALLINT,
+	codec		VARCHAR NOT NULL,
+	PRIMARY KEY (film_id, stream_id),
+	FOREIGN KEY (stream_id) REFERENCES films(film_id) ON UPDATE CASCADE ON DELETE CASCADE
+)
+
+CREATE TABLE stream_metadata_video (
+	film_id		INTEGER,
+	stream_id	SMALLINT,
+	frames		INTEGER NOT NULL,
+	PRIMARY KEY (film_id, stream_id),
+	FOREIGN KEY (stream_id) REFERENCES films(film_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE stream_metadata_video_resolution (
+	film_id		INTEGER,
+	stream_id	SMALLINT,
+	width		INTEGER NOT NULL,
+	height		INTEGER NOT NULL,
+	PRIMARY KEY (film_id, stream_id),
+	FOREIGN KEY (stream_id) REFERENCES films(film_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE stream_metadata_video_color (
+	film_id		INTEGER,
+	stream_id	SMALLINT,
+	prim 		VARCHAR NOT NULL,
+	matrix		VARCHAR NOT NULL,
+	transfer	VARCHAR NOT NULL,
+	pix_fmt		VARCHAR NOT NULL,
+	PRIMARY KEY (film_id, stream_id),
+	FOREIGN KEY (stream_id) REFERENCES films(film_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE stream_metadata_video_hdr10 (
+	film_id		INTEGER,
+	stream_id	SMALLINT,
+	red_x		INTEGER NOT NULL,
+	red_y		INTEGER NOT NULL,
+	green_x		INTEGER NOT NULL,
+	green_y		INTEGER NOT NULL,
+	blue_x		INTEGER NOT NULL,
+	blue_y		INTEGER NOT NULL,
+	white_x		INTEGER NOT NULL,
+	white_y		INTEGER NOT NULL,
+	lum_min		INTEGER,
+	lum_max		INTEGER,
+	light_max	INTEGER,
+	light_avg	INTEGER,
+	has_plus	BOOL NOT NULL,
+	PRIMARY KEY (film_id, stream_id),
+	FOREIGN KEY (stream_id) REFERENCES films(film_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE stream_metadata_subtitle_encoding (
+	film_id		INTEGER,
+	stream_id	SMALLINT,
+	encoding 	VARCHAR NOT NULL,
+	PRIMARY KEY (film_id, stream_id),
+	FOREIGN KEY (stream_id) REFERENCES films(film_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
