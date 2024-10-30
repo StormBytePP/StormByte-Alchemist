89879c310ee31eb21dfa1aa0a212eb2e95d157c7
David C. Manuelda <StormByte@gmail.com>
2024-10-30 05:02:22 +0100

    Updated Windows deploy script

--- deploy_windows.bat
+++ deploy_windows.bat
@@ -2,19 +2,18 @@
 
 rmdir /s /q build
 rmdir /s /q dist
-rmdir /s /q deps
 mkdir build
 mkdir dist
-mkdir deps
 
 git submodule update --init --recursive
 git submodule update --recursive --remote
 
 cd build
+mkdir deps
 mkdir libconfig
 cd libconfig
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR=Ninja -DCMAKE_INSTALL_PREFIX=..\..\deps -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF ..\..\thirdparty\libconfig
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR=Ninja -DCMAKE_INSTALL_PREFIX=..\deps -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF ..\..\thirdparty\libconfig
 ninja install
 
 cd ..
