1a1384901e383a1b5c463b57587f8997b3cd7e8e
David C. Manuelda <StormByte@gmail.com>
2024-10-30 05:33:40 +0100

    Preparing win build

--- deploy_windows.bat
+++ deploy_windows.bat
@@ -2,21 +2,21 @@
 
 rmdir /s /q build
 rmdir /s /q dist
+rmdir /s /q thirdparty\libconfig++\dist
+
 mkdir build
+mkdir build\libconfig++
 mkdir dist
+mkdir thirdparty\libconfig++\dist
 
 git submodule update --init --recursive
 git submodule update --recursive --remote
 
-cd build
-mkdir deps
-mkdir libconfig
-cd libconfig
-
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR=Ninja -DCMAKE_INSTALL_PREFIX=..\deps -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF ..\..\thirdparty\libconfig
+cmake -Bbuild\libconfig++ -Hthirdparty\libconfig++\src -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR=Ninja -DCMAKE_INSTALL_PREFIX=thirdparty\libconfig++\dist -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF
+cd build\libconfig++
 ninja install
 
 cd ..
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR=Ninja -DCMAKE_INSTALL_PREFIX=../dist -DWITH_SYSTEM_JSONCPP=OFF -DWITH_SYSTEM_SQLITE=OFF ..
+cmake -B. -H.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR=Ninja -DCMAKE_INSTALL_PREFIX=../dist -DWITH_SYSTEM_JSONCPP=OFF -DWITH_SYSTEM_SQLITE=OFF
 ninja install
 cd ..
