b9c1fd86b622a96ed8b7b76a53477ce8fbaf0c9b
David C. Manuelda <StormByte@gmail.com>
2024-11-10 20:23:45 +0100

    Use StormByte system variables

--- Alchemy/path.hxx
+++ Alchemy/path.hxx
@@ -9,17 +9,5 @@ namespace Alchemy {
 		public:
 			static const std::filesystem::path Home();
 			static const std::filesystem::path Conf();
-			static const std::filesystem::path Expand(const std::string&);
-			#ifdef WINDOWS
-			static const std::filesystem::path Expand(const std::wstring&);
-			#endif
-
-		private:
-			static const std::string			ExpandEnvironmentVariable(const std::string&);
-			#ifdef WINDOWS
-			static const std::string			ExpandEnvironmentVariable(const std::wstring&);
-			static std::string					UTF8Encode(const std::wstring&);
-			static std::wstring					UTF8Decode(const std::string&);
-			#endif
 	};
 }
